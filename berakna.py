1ecf4aa5849009d9feef7ab0a1cf104322b0d2e3
Anton <normananton03@gmail.com>
2021-09-01 20:25:30 +0200

    Beräknande funktion. Systemet fungerar som det ska, men saknar GUI, samt ett bra sätt att välja antal kurser som ska läggas till

--- berakna.py
+++ berakna.py
@@ -1,3 +1,4 @@
+from typing import AsyncGenerator
 from Kurs import Kurs
 
 kurser = []
@@ -12,6 +13,23 @@ def add_course():
     kurser.append(kurs)
     
 
+def calc_merit():
+    global kurser
+    total_points = 0
+    total_merit = 0
+    for kurs in kurser:
+        total_points += kurs.points
+        total_merit += kurs.calculate_score(kurs.points, kurs.grade)
+    average_score = total_merit/total_points
+    average_score = round(average_score, 2)
+    return average_score
+
+
+for i in range(16):
+    add_course()
+
+print("\n")
 
 for kurs in kurser:
     print(kurs.name)
+print("\n" + str(calc_merit()))
